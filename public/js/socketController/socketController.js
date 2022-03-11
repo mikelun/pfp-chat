65363fad791aab4289f2373d8655652bc4531a74
mikelun <erin788@mail.ru>
2022-03-11 10:14:45 +0800

    changed io

--- public/js/socketController/socketController.js
+++ public/js/socketController/socketController.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
  * Initialize socket and connect to server by socket.io
  */
  export function initializeSocket(self, peers) {
-    self.socket = io('ws://localhost:5000');
+    self.socket = io();
 
     // Initialize audio stream for socket
     initializeAudio(self.socket, peers, self);
