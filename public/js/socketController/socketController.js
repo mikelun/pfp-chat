78694f20e8daa0cb26d547f996cab8f0a6cf813f
Aleksey Bykhun <theisease@gmail.com>
2022-04-18 17:01:02 +0800

    draft

--- public/js/socketController/socketController.js
+++ public/js/socketController/socketController.js
@@ -3,12 +3,79 @@ import { initializePlayersSocket } from '../socketController/playerSocket';
 import { io } from "socket.io-client";
 import { currentPlayerDisconnected } from '../socketController/playerSocket';
 
+
+const connectWS = async () => {
+
+    // Initialize socket
+    // will not use tls if the connection is not made over https
+    const protocol = window.location.protocol.includes('https') ? 'wss' : 'ws'
+    const ws = new WebSocket(`${protocol}://${location.host}`);
+
+    const socket = {
+        id: null,
+        events: [],
+    }
+
+    socket.emit = (event, ...data) => {
+        console.log('emit', event, ...data);
+        ws.send(JSON.stringify({ event, data }));
+    }
+
+    socket.on = function (event, callback) {
+        console.log('on', event);
+        socket.events.push({ event, callback });
+    }
+
+    await new Promise((resolve, reject) => {
+        ws.onopen = () => {
+            console.log('Connected to server');
+        }
+
+        ws.onmessage = (_event) => {
+            const data = JSON.parse(_event.data);
+
+            if (data.id) {
+                socket.id = data.id;
+                console.log('received id', data.id);
+                resolve(data.id);
+            } else {
+                console.error('received unknown data instead of id', data);
+                reject('received unknown data instead of id');
+            }
+
+        }
+    });
+
+    ws.onmessage = (_event) => {
+        const data = JSON.parse(_event.data);
+
+        if (!data) { return }
+
+        if (!socket.id) {
+            console.error('received event before id', data.event, data.data);
+            return;
+        }
+
+        console.log('received event', data.event, ...data.data);
+
+        socket.events.forEach(({ event: _event, callback: _callback }) => {
+            if (data.event === _event) {
+                _callback(...data.data);
+            }
+        });
+    }
+
+    return socket
+}
+
 /**
  * Initialize socket and connect to server by socket.io
  */
-export function initializeSocket(self, peers) {
+export async function initializeSocket(self, peers) {
 
-    self.socket = io();    
+    // self.socket = io();
+    self.socket = await connectWS();
+    window.socket = self.socket;
 
     // Initialize audio stream for socket
     initializeAudio(self.socket, peers, self);
--- src/server.js
+++ src/server.js
@@ -16,10 +16,65 @@ var port = process.env.PORT || 8080; // set our port
 
 // build server
 const server = require('http').Server(app);
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ server });
+
 const io = require('socket.io')(server);
-require('./socketcontroller.js')(io)
 
+const { onConnect, ioConfig } = require('./socketcontroller.js');
 
+const buildSocket = (wss, ws) => {
+
+    // generate random id
+    const id = Math.random().toString(36).substring(7);
+
+    ws.send(JSON.stringify({ id }));
+
+    const socket = {
+        id,
+        emit: (event, ...data) => {
+            console.log('emit', event, ...data);
+            ws.send(JSON.stringify({ event, data }));
+        },
+        on: (event, callback) => {
+            console.log('on', event);
+
+            ws.on('message', (_event) => {
+                const data = JSON.parse(_event);
+
+                console.log('received event', data.event, ...data.data);
+
+                if (data.event === event) {
+                    callback(...data.data);
+                }
+
+            });
+        },
+        broadcast: {
+            emit: (event, ...data) => {
+                console.log('broadcast emit', event, ...data)
+
+                wss.clients.forEach(client => {
+                    if (client === ws) {
+                        // skip broadcast to yourself
+                        return
+                    }
+
+                    client.send(JSON.stringify({ event, data }));
+                });
+            }
+        }
+    };
+
+    return socket;
+}
+
+wss.on('connection', (ws) => {
+    const socket = buildSocket(wss, ws);
+
+    onConnect(socket);
+});
 
 server.listen(port, function(){
     console.log('listening on *:' + port);
