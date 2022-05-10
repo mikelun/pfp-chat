74b3f750f35ead9c595d8e9c41aa41975916149c
mikelun <erin788@mail.ru>
2022-05-10 11:18:49 +0800

    fixed text chat

--- public/js/scenes/GameUI-elements/textChat.js
+++ public/js/scenes/GameUI-elements/textChat.js
@@ -5,7 +5,7 @@ import BBCodeText from 'phaser3-rex-plugins/plugins/bbcodetext.js';
 import { blockMovement, unblockMovement } from "../../utils/utils";
 import { sendTextChatMessage } from "../../socketController/textChatSocket";
 
-const defaultText = 'Type your message here';
+const defaultText = '';
 
 var sizer;
 var self;
@@ -22,24 +22,6 @@ export function addChat(newSelf) {
         width: 400, height: 400,
         orientation: 'y',
     });
-    // if player tap enter 
-    self.input.keyboard.on('keydown-ENTER', function (event) {
-        if (self.printText.alpha == 1) {
-            var text = self.printText.text;
-            if (text.length > 0 && text != defaultText) {
-                const message = self.playerName + ': ' + text;
-                addMessage(message, true);
-            } else {
-                self.printText.text = defaultText;
-                self.printText.alpha = 0;
-                self.tipsText.alpha = 0;
-            }
-        } else {
-            self.printText.alpha = 1;
-            self.tipsText.alpha = 1;
-        }
-    });
-    // const child = this.addTextBox('asdfasdf');
 }
 
 export function addMessage(message, sendToServer) {
@@ -49,7 +31,7 @@ export function addMessage(message, sendToServer) {
     }
     const child = addTextBox(self, message);
     sizer.add(child).layout();
-    
+
     if (sendToServer) sendTextChatMessage(message);
     // remove this child after 10 seconds
     self.time.delayedCall(10000, function () {
@@ -58,47 +40,60 @@ export function addMessage(message, sendToServer) {
 }
 
 function addEditText() {
-        self.tipsText = self.add.text(500, 530, 'To close the chat, press enter', { fontSize: '35px', fill: "#ffffff", fontFamily: 'PixelFont' }).setAlpha(0);
-        self.printText = new BBCodeText(self, 700, 500, defaultText, {
-            color: '#ffffff',
-            fontSize: '35px',
-            fixedWidth: 800,
-            fixedHeight: 50,
-            backgroundColor: "#333366",
-            valign: 'center',
-            fontFamily: 'PixelFont',
-            padding: {
-                left: 20,
-                right: 0,
-                top: 0,
-                bottom: 10,
-            },
-        })
-            .setOrigin(0.5)
-            .setInteractive()
-            .on('pointerdown', function () {
-                console.log("HERE");
-                var config = {
-                    enterClose: true,
-                    onOpen: function (printText) {
-                        blockMovement();
-                        //self.tipsText.alpha = 0;
-                    },
-                    onTextChanged: function (printText, text) {
-                        printText.text = text;
-                    },
-                    onClose: function (printText) {
-                        unblockMovement();
-                        self.printText.text = '';
-                    },
-                    selectAll: true,
-                    // enterClose: false
-                }
-                var editor = new TextEdit(self.printText);
-                editor.open(config);
-            }, self);
-        self.add.existing(self.printText);
-        self.printText.setAlpha(0);
-    }
+    self.tipsText = self.add.text(470, 530, 'To close the chat, click on any place', { fontSize: '35px', fill: "#ffffff", fontFamily: 'PixelFont' }).setAlpha(0);
+    self.printText = new BBCodeText(self, 700, 500, defaultText, {
+        color: '#ffffff',
+        fontSize: '35px',
+        fixedWidth: 800,
+        fixedHeight: 50,
+        backgroundColor: "#333366",
+        valign: 'center',
+        fontFamily: 'PixelFont',
+        padding: {
+            left: 20,
+            right: 0,
+            top: 0,
+            bottom: 10,
+        },
+    }).setOrigin(0.5).setAlpha(0);
+
+    self.input.keyboard.on('keydown', function (event) {
+        if (event.key == 'Enter') {
+            if (self.printText.alpha == 1)  {
+                self.printText.alpha = 0;
+            } else {
+                self.printText.alpha = 1;
+            }
+        }
+        if (self.printText.alpha == 1 && (self.printText.text == defaultText || self.printText.text.length == 0)) {
+            var config = {
+                enterClose: true,
+                onOpen: function (printText) {
+                    blockMovement();
+                    self.tipsText.alpha = 1;
+                    //self.tipsText.alpha = 0;
+                },
+                onTextChanged: function (printText, text) {
+                    printText.text = text;
+                },
+                onClose: function (printText) {
+                    self.tipsText.alpha = 0;
+                    self.printText.alpha = 0;
+                    const text = self.printText.text;
+                    if (text.length > 0 && text != defaultText && !self.input.activePointer.isDown) {
+                        const message = self.playerName + ': ' + text;
+                        addMessage(message, true);
+                    }
+                    unblockMovement();
+                    self.printText.text = '';
+                },
+                selectAll: true,
+                // enterClose: false
+            }
+            var editor = new TextEdit(self.printText);
+            editor.open(config);
+        }
+    }, self);
+}
 
 
