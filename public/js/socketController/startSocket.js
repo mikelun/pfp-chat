3682dc9f32cd82ac5956d088ff71e7138dbb4fd8
mikelun <erin788@mail.ru>
2022-06-06 13:15:35 +0800

    trying again

--- public/js/socketController/startSocket.js
+++ public/js/socketController/startSocket.js
@@ -2,11 +2,10 @@ import { io } from "socket.io-client";
 
 export function startSocket() {
     const port = process.env.PORT || 3000;
-    const site = '137.184.233.127/'
+    const site = window.location.hostname;
     const connectLink = site == 'localhost' ? `ws://localhost:${port}` : `ws://${site}`;
     
     const socket = io(connectLink, { transports: ['websocket']});
 
-    console.log(connectLink);
     return socket;
 }
