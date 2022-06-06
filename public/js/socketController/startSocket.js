2ea337b38e7d9317dc61d0ff18a61f0e1e309260
mikelun <erin788@mail.ru>
2022-06-06 13:13:55 +0800

    changed site name

--- public/js/socketController/startSocket.js
+++ public/js/socketController/startSocket.js
@@ -2,10 +2,11 @@ import { io } from "socket.io-client";
 
 export function startSocket() {
     const port = process.env.PORT || 3000;
-    const site = window.location.hostname;
+    const site = '137.184.233.127/'
     const connectLink = site == 'localhost' ? `ws://localhost:${port}` : `ws://${site}`;
     
     const socket = io(connectLink, { transports: ['websocket']});
 
+    console.log(connectLink);
     return socket;
 }
