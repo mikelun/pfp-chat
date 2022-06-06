d6b10cb9daeada23658f6816e27b8ea7202ece89
mikelun <erin788@mail.ru>
2022-06-06 13:10:01 +0800

    changed wss to ws

--- public/js/socketController/startSocket.js
+++ public/js/socketController/startSocket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 export function startSocket() {
     const port = process.env.PORT || 3000;
     const site = window.location.hostname;
-    const connectLink = site == 'localhost' ? `ws://localhost:${port}` : `wss://${site}`;
+    const connectLink = site == 'localhost' ? `ws://localhost:${port}` : `ws://${site}`;
     
     const socket = io(connectLink, { transports: ['websocket']});
 
